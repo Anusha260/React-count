1638f96b7560333bb0341dd24701ff3dac0fb8cb
Anusha260 <anusha20@navgurukul.org>
2022-12-29 12:04:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-const [count, setCount] = useState(0); 
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-  return (
-    <div className="app">
-      <center>
-      <button onClick={incrementCount}>Click Here</button><br></br>
-      <h1 style={{color:"blue"}}>{count}</h1>
-      </center>
-    </div>
-  );
+  const list=[{name:"Anusha",age:22},{name:"Anu",age:21}]
+
+return(
+  <div>
+    {
+      list.map((event,index)=>{
+        return(
+          <div>
+            <table>
+              <th>Name</th>
+              <th>Age</th>
+              <tr>
+                <ul>
+                  <li>
+                    <h1>{event.name}</h1>
+
+                  </li>
+                  <li>
+                    <h1>{event.age}</h1>
+
+                  </li>
+                </ul>
+              </tr>
+            </table>
+          </div>
+        )
+      })
+    }
+
+  </div>
+)
+
 }
 
 export default App
